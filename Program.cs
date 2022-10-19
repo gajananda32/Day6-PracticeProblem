ee6abb6c25329f74fc211e5d0703e352ed6f13c2
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-19 11:50:34 +0530

    PrimeNumber

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-﻿Console.WriteLine("Enter a number: ");
+//taking input from user﻿
+Console.WriteLine("Enter a number: ");
+//converting string to integer
 int num = int.Parse(Console.ReadLine());
+//count variable to count prime number
 int count = 0;
 for (int i = 1; i <= num; i++)
 {
     if (num % i == 0)
     {
+        //counting the number
         count++;
     }
 }
+//checking prime number
 if (count == 2)
 {
     Console.WriteLine(num + " is a prime number");
