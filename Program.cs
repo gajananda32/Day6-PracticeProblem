5b7c0b358d6992a9fa86ed64f6882ac81eac4f83
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-19 11:51:28 +0530

    PerfectNumber

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿Console.WriteLine("Enter the number: ");
+//taking input from user﻿
+Console.WriteLine("Enter the number: ");
+//converting string to integer
 int num = int.Parse(Console.ReadLine());
 int total = 0;
 for(int i = 1; i < num; i++)
 {
+    //checking for perfect number;
     if (num % i == 0)
     {
         total += i;
