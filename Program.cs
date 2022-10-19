c6fdd376076428271430cc25e2f8608fcbe30b18
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-19 11:43:15 +0530

    Reversing the Number

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+int rev = 0;
+int rem = 0;
+while (num > 0)
+{
+    rem = num % 10;
+    rev = rev * 10 + rem;
+    num = (num / 10);
+}
+Console.WriteLine(rev);
