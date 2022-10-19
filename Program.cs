3d8516224907697549659641fb480f74757c94c4
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-19 11:41:45 +0530

    PrimeNumber

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 1; i <= num; i++)
+{
+    if (num % i == 0)
+    {
+        count++;
+    }
+}
+if (count == 2)
+{
+    Console.WriteLine(num + " is a prime number");
+}
+else
+{
+    Console.WriteLine(num + " is not a prime number");
+}
