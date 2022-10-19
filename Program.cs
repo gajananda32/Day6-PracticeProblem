1e4d4f881cb9da784c8d51d5041a1e352e303e87
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-19 11:39:12 +0530

    Fibonicci series

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//variables
+int a = 0;
+int b=1;
+int c=0;
+Console.WriteLine("Fibonacci Series : ");
+Console.WriteLine(a + " " + b + " ");
+//intilizing for loop
+for(int i = 0; i <= 10; i++)
+{
+    c = a + b;
+    Console.WriteLine(c);
+    a = b;
+    b = c;
+}
+
+
+
