4156b1a96b39b44c6ff42f22aea32b1885a40dbf
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-19 11:40:26 +0530

    Adding PerfrectNumber

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter the number: ");
+int num = int.Parse(Console.ReadLine());
+int total = 0;
+for(int i = 1; i < num; i++)
+{
+    if (num % i == 0)
+    {
+        total += i;
+    }
+}
+if (total == num)
+{
+    Console.WriteLine(num + " is a perfect number");
+}
+else
+{
+    Console.WriteLine(num + " is not a perfect number");
+}
