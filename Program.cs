76f9eff9f0e2c58f6cabe9e2c9ffd43aff730df1
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-19 11:46:30 +0530

    Vending Machine

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter the Money: ");
+int n = int.Parse(Console.ReadLine());
+int[] arr = { 1, 2, 5, 10, 20, 50, 100, 500, 1000 };
+int count = 0;
+for(int i = arr.Length-1; i >= 0; i--)
+{
+    if (arr[i] <= n)
+    {
+        count += n / arr[i];
+        n %= arr[i];
+    }
+}
+Console.WriteLine(count);
+
